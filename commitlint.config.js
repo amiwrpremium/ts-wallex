55fbadc479ec988ac4e3b3e02861b2d3304938d2
AMiWR <amiwrpremium@gmail.com>
2024-02-18 15:29:17 +0330

    chore: update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // add `release` to the list of allowed types
+    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "perf", "test", "chore", "release"]],
+  }
+};
+
+module.exports = config;
