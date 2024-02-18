fa0dacc80bc6487dcbd80f0a42a853154aa440dd
AMiWR <amiwrpremium@gmail.com>
2024-02-18 19:40:34 +0330

    chore: fix build stuff

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,8 +2,22 @@ const config = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     // add `release` to the list of allowed types
-    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "perf", "test", "chore", "release"]],
-  }
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "release",
+      ],
+    ],
+  },
 };
 
 module.exports = config;
